7f9f686a6ffcd5206121381ddd847b26de4093b0
Filipe Taborda <filipe.taborda92@gmail.com>
2023-10-30 22:54:17 +0000

    feature: adding header mobile mode and footer

--- src/components/Section/Section.tsx
+++ src/components/Section/Section.tsx
@@ -1,9 +1,13 @@
+import { PropsWithChildren } from "react";
+
 export interface SectionProps {
   name: string;
-  children: any;
 }
 
-export default function Section({ name, children }: SectionProps) {
+export default function Section({
+  name,
+  children,
+}: PropsWithChildren<SectionProps>) {
   return (
     <div className="h-full w-full flex flex-col items-center justify-center px-5 md:px-15">
       <h2 className="font-bold text-3xl pb-10">{name}</h2>

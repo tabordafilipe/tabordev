0782077df700c6ac2bc5e0b0102fd3b040a382c8
Filipe Taborda <filipe.taborda92@gmail.com>
2023-11-02 12:43:47 +0000

    bugfix: mobile menu - scroll and footer

--- src/components/Drawer/Drawer.tsx
+++ src/components/Drawer/Drawer.tsx
@@ -1,4 +1,4 @@
-import React, { PropsWithChildren, ReactNode } from "react";
+import React, { PropsWithChildren, ReactNode, useEffect } from "react";
 import styles from "./Drawer.module.scss";
 import { useDarkMode } from "usehooks-ts";
 import Icon from "../Icon/Icon";
@@ -24,6 +24,14 @@ export default function Drawer({
 }: PropsWithChildren<DrawerProps>) {
   const { isDarkMode } = useDarkMode();
   const anchorClassName = styles[`Drawer_${anchor}`];
+
+  useEffect(() => {
+    if (open) {
+      document.body.classList.add("drawer-open");
+    } else {
+      document.body.classList.remove("drawer-open");
+    }
+  }, [open]);
 
   return (
     <>

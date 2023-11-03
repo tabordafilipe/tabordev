863d537efd0df05e23a7ba792d6ccffc687a8374
Filipe Taborda <filipe.taborda92@gmail.com>
2023-11-03 17:40:32 +0000

    refactor: section component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,14 +2,21 @@ import styles from "./Footer.module.scss";
 
 export default function Footer() {
   return (
-    <footer className={`${styles.Footer} w-full p-4 text-sm`}>
-      <div className="text-center">
-        <p>
+    <footer className={`${styles.Footer} w-full py-4 px-4 md:px-12 text-sm`}>
+      <div className="flex justify-between">
+        <div>
           {"© 2023 - "}
           <a className="font-semibold" href="mailto:tabordev.code@gmail.com">
             tabordev
           </a>
-        </p>
+        </div>
+        <a
+          className="font-semibold"
+          target="_blank"
+          href="https://github.com/tabordafilipe/tabordev-cv"
+        >
+          Source code 
+        </a>
       </div>
     </footer>
   );

6799f0f2c02d442e79aeec8f6fd59f91260adc47
Filipe Taborda <filipe.taborda92@gmail.com>
2023-11-06 16:33:18 +0000

    feature: adding borders in all skills circles

--- src/components/Skills/Skill/Skill.tsx
+++ src/components/Skills/Skill/Skill.tsx
@@ -1,5 +1,9 @@
+import { useDarkMode } from "usehooks-ts";
 import styles from "./Skill.module.scss";
 import SkillImage from "./SkillImage";
+import { useEffect, useState } from "react";
+
+const CLASSIFICATION_MAX_LENGTH: number = 5;
 
 export interface SkillProps {
   id: string;
@@ -8,29 +12,24 @@ export interface SkillProps {
   classification: number;
 }
 
-function buildSkillCircles(id: string, classification: number) {
+function getSkillClassification(
+  id: string,
+  classification: number,
+  isDarkMode: boolean
+) {
   const roundClassification = Math.floor(classification);
 
-  let skills = [];
+  return Array.from(Array(CLASSIFICATION_MAX_LENGTH).keys()).map((value) => {
+    let classStr = isDarkMode ? "Skill__Circle--dark" : "Skill__Circle";
+    if (value < roundClassification) {
+      classStr += "_Filled";
+    } else if (value < classification && classification > roundClassification) {
+      classStr += "_HalfFilled";
+    }
+    let className = styles[classStr];
 
-  let n = 0;
-  while (n < roundClassification) {
-    skills.push(
-      <div key={`${id}-circle-${n}`} className={styles.Skill__Circle}></div>
-    );
-    n++;
-  }
-
-  if (classification > roundClassification) {
-    skills.push(
-      <div
-        key={`${id}-circle-half`}
-        className={styles.Skill__Circle_Half}
-      ></div>
-    );
-  }
-
-  return skills;
+    return <div key={`${id}-circle-${value}`} className={className}></div>;
+  });
 }
 
 export default function Skill({
@@ -39,15 +38,26 @@ export default function Skill({
   description,
   classification,
 }: SkillProps) {
-  const skills = buildSkillCircles(id, classification);
+  const { isDarkMode } = useDarkMode();
+  const [skillClassification, setSkillClassification] = useState<
+    JSX.Element[] | null
+  >(null);
+
+  useEffect(
+    () =>
+      setSkillClassification(
+        getSkillClassification(id, classification, isDarkMode)
+      ),
+    [classification, id, isDarkMode]
+  );
 
   return (
     <div id={id} className="w-full flex flex-row items-center justify-center">
-      <div className="w-32 flex flex-col items-center p-2 md:p-5 mr-4 md:mr-5">
+      <div className="w-32 flex flex-col items-center p-2 md:p-5 mr-2 md:mr-5">
         <SkillImage name={name} description={description} />
       </div>
       <div className="w-60 mb-4 space-x-5 flex flex-row items-center">
-        {skills}
+        {skillClassification}
       </div>
     </div>
   );

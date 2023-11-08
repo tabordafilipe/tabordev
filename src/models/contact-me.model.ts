6ed79ad7c33e81a19755e9df7eb56031dac9fabd
Filipe Taborda <filipe.taborda92@gmail.com>
2023-11-08 18:20:12 +0000

    feature: create contact me component

---
+++ src/models/contact-me.model.ts
@@ -0,0 +1,9 @@
+import { IconInfo } from "./info.model";
+
+export interface ContactMeInfo {
+  title: string;
+  subtitle: string;
+  email: string;
+  emailDescription: string;
+  icons: IconInfo[];
+}

997733af8f94b26ff5aec9bf794c0538fca77e30
Filipe Taborda <filipe.taborda92@gmail.com>
2023-11-11 11:40:26 +0000

    feature: adding Curriculum component to Home component

--- src/modules/Home/Home.tsx
+++ src/modules/Home/Home.tsx
@@ -1,11 +1,12 @@
 import { useContext, useState } from "react";
 
 import About from "@/components/About/About";
-import Background from "@/components/Background/Background";
 import ContactMe from "@/components/ContactMe/ContactMe";
 import INFO from "@/assets/configs/info.json";
+import Icon from "@/components/Icon/Icon";
 import { Info } from "@/models/info.model";
 import { IsLogoVisibleContext } from "@/hooks/IsVisible";
+import Link from "next/link";
 import MyInfo from "@/components/MyInfo/MyInfo";
 import SECTIONS from "@/assets/configs/sections.json";
 import Section from "@/components/Section/Section";
@@ -31,7 +32,6 @@ export default function Home() {
 
   return (
     <main className="font-mono">
-      <Background />
       {!isMobile && (
         <div className={styles.Home__ThemeSwitcher}>
           <ThemeSwitcher />
@@ -61,12 +61,36 @@ export default function Home() {
       </Section>
 
       {/* TIMELINE section */}
-      <Section id={timeline.id} name={timeline.name}>
+      <Section id={timeline.id} name={timeline.name} className="pb-32">
         <Timeline />
       </Section>
 
+      {/* CURRICULUM section */}
+      <section
+        className={`${styles.Home__Section__Curriculum} block bg-opacity-40 bg-green-10 hover:bg-green-25 hover:bg-opacity-70 dark:bg-opacity-75 dark:bg-black-25`}
+      >
+        <Link href="/curriculum">
+          <span className={`${styles.Home__Section__Curriculum__Text} text-xl`}>
+            View and download my full CV
+          </span>
+          <Icon
+            className={styles.Home__Section__Curriculum__Icon}
+            name="download"
+            title="View and download my full Curriculum"
+            dimensions={{
+              width: 50,
+              height: 50,
+            }}
+          />
+        </Link>
+      </section>
+
       {/* CONTACT ME section */}
-      <Section id={contactme.id} name={contactme.name} className={styles.Home__Section__ContactMe}>
+      <Section
+        id={contactme.id}
+        name={contactme.name}
+        className={styles.Home__Section__ContactMe}
+      >
         <ContactMe />
       </Section>
     </main>

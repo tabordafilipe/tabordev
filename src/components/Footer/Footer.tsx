01b0e7f28090ca44bd1c94c36857bca49c6ed49e
Filipe Taborda <filipe.taborda92@gmail.com>
2023-12-07 10:05:03 +0000

    feature: update footer source code link

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,11 +1,12 @@
 import styles from "./Footer.module.scss";
 
 export default function Footer() {
+  const currentYear = new Date().getFullYear();
   return (
     <footer className={`${styles.Footer} w-full py-4 px-4 md:px-12 text-sm`}>
       <div className="flex justify-between">
         <div>
-          {"© 2023 - "}
+          {`© ${currentYear} - `}
           <a className="font-semibold" href="mailto:tabordev.code@gmail.com">
             tabordev
           </a>
@@ -13,9 +14,9 @@ export default function Footer() {
         <a
           className="font-semibold"
           target="_blank"
-          href="https://github.com/tabordafilipe/tabordev-cv"
+          href="https://github.com/tabordafilipe/tabordev"
         >
-          Source code 
+          Source code
         </a>
       </div>
     </footer>
